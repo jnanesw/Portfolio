8bcbcd5c15c1e976c0bd2665991f17744d3acc44
jnanesw <nanimca000@gmail.com>
2025-06-26 00:04:20 +0530

    Full Responsive Portfolio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <nav className="navbar scroll-smooth">
         <div className="nav-container">
-            <div className="logo"><img src={logo} className="transition-transform duration-500 rounded-full hover:hue-rotate-60 hover:rotate-45" /></div>
+            <div className="logo"><img src={logo} className="transition-transform duration-500 rounded-full hover:hue-rotate-60 hover:rotate-45 sm:block hidden" /></div>
             <div className="nav-links">
                 <Link to="/">Home</Link>
                 <Link to="/skills">Skills</Link>
